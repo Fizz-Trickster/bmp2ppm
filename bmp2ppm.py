5fea54c782449fa837cfb564499b5348f3c0035d
yoonys_desktop <yyd1109@gmail.com>
2022-02-04 02:34:44 +0900

    bmp2ppm.py update

--- bmp2ppm.py
+++ bmp2ppm.py
@@ -53,7 +53,6 @@ def getDecodeData(i_data, BYTE_SIZE):
 # ========================================
 
 # file = open('./image/flag.bmp', 'rb')
-# file = open('./image/lena.bmp', 'rb')
 file = open('./image/24bpp-320x240.bmp', 'rb')
 
 bindata = file.read()
@@ -87,29 +86,17 @@ Rdata_2d_rev = np.flip(Rdata_2d, axis=0)
 Gdata_2d_rev = np.flip(Gdata_2d, axis=0)
 Bdata_2d_rev = np.flip(Bdata_2d, axis=0)
 
+
 ppmFile = open('./output.ppm', 'w')
 
 ppmFile.write('P3\n')
 ppmFile.write('{0} {1}\n'.format(bfHeader['Hres'], bfHeader['Vres']))
 ppmFile.write('255\n')
 
-for Vidx in range(0,bfHeader['Vres']):
+for Vidx in range(0, bfHeader['Vres']):
   for Hidx in range(0, bfHeader['Hres']):
     ppmFile.write('{0} {1} {2}\n'.format(Rdata_2d_rev[Vidx][Hidx], Gdata_2d_rev[Vidx][Hidx], Bdata_2d_rev[Vidx][Hidx]))
     
   
-# hex to ascii
-l = chr(hex2dec(bfHeader['bfType'][:2]))
-m = chr(hex2dec(bfHeader['bfType'][2:]))
-print(l+m)
-
-
-listData = list(strdata)
-
-a = np.array(listData)
-b = a.reshape(-1, 2)
-print(b[0])
-print(type(b[0]), type(b[0][0]))
-
 file.close()
 ppmFile.close()
